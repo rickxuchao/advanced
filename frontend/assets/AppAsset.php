038c1dfd4c2ee51ec685bcf815e625c35c12bc59
rick <xuchao@erahealth.cn>
2016-08-18 22:39:27 +0800

    0818-5

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -11,7 +11,7 @@ class AppAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
-    public $css = [dgfdfg
+    public $css = [
         'css/site.css',
     ];
     public $js = [
